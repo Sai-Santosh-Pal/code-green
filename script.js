c5f19c417a5fb90170ab2a0822b57cc7db4f9189
Sai Santosh Pal <saisantoshpal2010@gmail.com>
2026-09-24 11:40:52 +0530

    mobile repsonsive bug fixes which had arisen due to scroll trigger

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
         trigger: ".line",
         start: "center center",
         end: () => "+=" + (horizontalSection.scrollWidth),
-        pin: ".parent",
+        pin: ".gallery",
         scrub: 1, 
         invalidateOnRefresh: true
     }
